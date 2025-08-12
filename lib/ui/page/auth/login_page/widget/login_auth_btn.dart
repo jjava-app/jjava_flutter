e412e0a1179dcf2f4115c8442b59824b4d9ac548
hoejeong <seohoejeong@gmail.com>
2025-08-12 16:06:33 +0900

    로그인 레이아웃 완료

---
+++ lib/ui/page/auth/login_page/widget/login_auth_btn.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class LoginAuthBtn extends StatelessWidget {
+  final Color btnColor;
+  final Widget socialLogo;
+  final String socialName;
+  final Color textColor;
+  final List<BoxShadow>? boxShadow;
+
+  const LoginAuthBtn({
+    super.key,
+    required this.btnColor,
+    required this.socialLogo,
+    required this.socialName,
+    required this.textColor,
+    this.boxShadow,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(4),
+        color: btnColor,
+        boxShadow: boxShadow,
+      ),
+      width: double.infinity,
+      height: 44,
+      child: InkWell(
+        onTap: () {},
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            children: [
+              socialLogo,
+              Expanded(
+                child: Text(
+                  socialName,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: textColor,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
