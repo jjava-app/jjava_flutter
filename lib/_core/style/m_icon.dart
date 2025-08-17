a760746f20fd3b74d587415c6745b211cb66b32a
hahamik <dlapdl0615@gmail.com>
2025-08-17 18:29:17 +0900

    워크스페이스 리스트  ui

--- lib/_core/style/m_icon.dart
+++ lib/_core/style/m_icon.dart
@@ -13,7 +13,9 @@ class _Nav {
 }
 
 class _Top {
-  final Widget arrowBack = SvgPicture.asset('assets/icons/nav_top_arrow_back.svg');
+  final Widget arrowBack = SvgPicture.asset(
+    'assets/icons/nav_top_arrow_back.svg',
+  );
   final Widget logo = SvgPicture.asset('assets/images/global/main_logo.svg');
   final Widget profile = SvgPicture.asset('assets/images/profile.svg');
 }
@@ -58,41 +60,78 @@ class _Page {
 }
 
 class _Home {
-  final Widget arrowForward = SvgPicture.asset('assets/icons/page_home_arrow_forward.svg', width: 5, height: 9);
-  final Widget arrowUpper = SvgPicture.asset('assets/icons/page_home_arrow_upper.svg');
-  final Widget codeSquare = SvgPicture.asset('assets/icons/page_home_code_square.svg');
+  final Widget arrowForward = SvgPicture.asset(
+    'assets/icons/page_home_arrow_forward.svg',
+    width: 5,
+    height: 9,
+  );
+  final Widget arrowUpper = SvgPicture.asset(
+    'assets/icons/page_home_arrow_upper.svg',
+  );
+  final Widget codeSquare = SvgPicture.asset(
+    'assets/icons/page_home_code_square.svg',
+  );
   final Widget logo = SvgPicture.asset('assets/images/global/main_logo.svg');
   final Widget block = SvgPicture.asset('assets/icons/page_home_block.svg');
 }
 
 class _Login {
-  final Widget naver = SvgPicture.asset('assets/icons/page_login_naver_logo.svg');
-  final Widget kakao = SvgPicture.asset('assets/icons/page_login_kakao_logo.svg');
-  final Widget google = SvgPicture.asset('assets/icons/page_login_google_logo.svg');
+  final Widget naver = SvgPicture.asset(
+    'assets/icons/page_login_naver_logo.svg',
+  );
+  final Widget kakao = SvgPicture.asset(
+    'assets/icons/page_login_kakao_logo.svg',
+  );
+  final Widget google = SvgPicture.asset(
+    'assets/icons/page_login_google_logo.svg',
+  );
 }
 
 class _MyPage {
   final Widget logout = SvgPicture.asset('assets/icons/page_mypage_logout.svg');
-  final Widget naver = SvgPicture.asset('assets/icons/page_mypage_naver_logo.svg');
+  final Widget naver = SvgPicture.asset(
+    'assets/icons/page_mypage_naver_logo.svg',
+  );
 }
 
 class _Question {
-  final Widget square = SvgPicture.asset('assets/icons/page_question_square.svg');
-  final Widget warning1 = SvgPicture.asset('assets/icons/page_question_warning1.svg');
-  final Widget warning2 = SvgPicture.asset('assets/icons/page_question_warning2.svg');
-  final Widget polygon = SvgPicture.asset('assets/icons/page_question_polygon.svg');
-  final Widget destructive = SvgPicture.asset('assets/icons/page_question_destructive.svg');
+  final Widget square = SvgPicture.asset(
+    'assets/icons/page_question_square.svg',
+  );
+  final Widget warning1 = SvgPicture.asset(
+    'assets/icons/page_question_warning1.svg',
+  );
+  final Widget warning2 = SvgPicture.asset(
+    'assets/icons/page_question_warning2.svg',
+  );
+  final Widget polygon = SvgPicture.asset(
+    'assets/icons/page_question_polygon.svg',
+  );
+  final Widget destructive = SvgPicture.asset(
+    'assets/icons/page_question_destructive.svg',
+  );
 }
 
 class _SolvedQuestion {
-  final Widget copy = SvgPicture.asset('assets/icons/page_solved_question_copy.svg');
+  final Widget copy = SvgPicture.asset(
+    'assets/icons/page_solved_question_copy.svg',
+  );
 }
 
 class _Workspace {
   final Widget copy = SvgPicture.asset('assets/icons/page_worksapce_copy.svg');
-  final Widget arrowRight = SvgPicture.asset('assets/icons/page_workspace_arrow_right.svg');
-  final Widget delete = SvgPicture.asset('assets/icons/page_workspace_delete.svg');
-  final Widget plusSquare = SvgPicture.asset('assets/icons/page_workspace_plus_square.svg');
+  final Widget arrowRight = SvgPicture.asset(
+    'assets/icons/page_workspace_arrow_right.svg',
+  );
+  final Widget delete = SvgPicture.asset(
+    'assets/icons/page_workspace_delete.svg',
+  );
+  Widget plusSquare({double? size, Color? color}) => SvgPicture.asset(
+    'assets/icons/page_workspace_plus_square.svg',
+    width: size,
+    height: size,
+    color: color,
+  );
 }
 
 class _Global {
