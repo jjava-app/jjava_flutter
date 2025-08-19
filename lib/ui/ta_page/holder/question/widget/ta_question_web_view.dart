df75055f7441fd888eed1c78e241ac6915af2d39
hoejeong <seohoejeong@gmail.com>
2025-08-19 14:09:42 +0900

    웹뷰 진행중

--- lib/ui/ta_page/holder/question/widget/ta_question_web_view.dart
+++ lib/ui/ta_page/holder/question/widget/ta_question_web_view.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:jjava_flutter/ui/ta_page/holder/question/widget/ta_question_block_dashboard.dart';
 
-class TaQuestionWebView extends StatelessWidget {
+class TaQuestionWebView extends StatefulWidget {
   const TaQuestionWebView({
     super.key,
   });
 
   @override
+  State<TaQuestionWebView> createState() => _TaQuestionWebViewState();
+}
+
+class _TaQuestionWebViewState extends State<TaQuestionWebView> {
+  @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Expanded(child: TaQuestionBlockDashboard()),
-          ],
-        ),
-      ],
-    );
+    return TaQuestionBlockDashboard();
   }
 }
