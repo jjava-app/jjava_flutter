3e3194a8e642269bac1a6b594156057bd73a6802
hoejeong <seohoejeong@gmail.com>
2025-08-19 14:14:35 +0900

    merge dev

--- lib/ui/ma_page/splash/ma_splash_page.dart
+++ lib/ui/ma_page/splash/ma_splash_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:jjava_flutter/ui/ma_page/holder/question/ma_question_page.dart';
+import 'package:jjava_flutter/ui/ma_page/holder/workspace/ma_workspace_page.dart';
 
 class MaSplashPage extends StatelessWidget {
   @override
