dcbf83231f6a85ac84baa66092efed3ce3ed10dd
hahamik <dlapdl0615@gmail.com>
2025-08-22 17:26:32 +0900

    내 변경 저장

--- lib/ui/ta_page/holder/home/widget/ta_home_my_rank.dart
+++ lib/ui/ta_page/holder/home/widget/ta_home_my_rank.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:jjava_flutter/_core/style/m_color.dart';
 import 'package:jjava_flutter/_core/style/m_text.dart';
+import 'package:jjava_flutter/data/model/user.dart';
 
 class TaHomeMyRank extends StatelessWidget {
+  final User user;
+
   const TaHomeMyRank({
     super.key,
+    required this.user,
   });
 
   @override
@@ -15,7 +19,7 @@ class TaHomeMyRank extends StatelessWidget {
         color: MColor.kFill.normal,
         borderRadius: BorderRadius.circular(8),
       ),
-      padding: const EdgeInsets.symmetric(horizontal: 16),
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -23,15 +27,15 @@ class TaHomeMyRank extends StatelessWidget {
           Row(
             children: [
               MText.s14Bold("내 랭킹", color: MColor.kLabel.neutral),
-              SizedBox(width: 16),
-              MText.s14Bold("DevSsar", color: MColor.kPrimary.normal),
+              const SizedBox(width: 16),
+              MText.s14Bold(user.username ?? '', color: MColor.kPrimary.normal),
             ],
           ),
           Row(
             children: [
-              _RankChip(label: "155 위"),
+              _RankChip(label: "${user.rank} 위"),
               const SizedBox(width: 8),
-              _RankChip(label: "2530점"),
+              _RankChip(label: "${user.score} 점"),
             ],
           ),
         ],
--- lib/ui/ta_page/holder/home/widget/ta_home_week_rank.dart
+++ lib/ui/ta_page/holder/home/widget/ta_home_week_rank.dart
@@ -1,90 +1,91 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:jjava_flutter/_core/style/m_color.dart';
 import 'package:jjava_flutter/_core/style/m_text.dart';
-import 'package:jjava_flutter/ui/vm/home_vm.dart';
+import 'package:jjava_flutter/data/model/week_rank.dart';
 
-class TaHomeWeekRank extends ConsumerWidget {
-  const TaHomeWeekRank({super.key});
+class TaHomeWeekRank extends StatelessWidget {
+  final List<WeekRank> rankingList;
+
+  const TaHomeWeekRank({
+    super.key,
+    required this.rankingList,
+  });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final homeModel = ref.watch(homeProvider);
-    if (homeModel == null) {
-      return Center(child: CircularProgressIndicator());
-    } else {
-      final rankingList = homeModel.weekRank;
-      return Container(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            MText.h2("이번 주 랭킹", color: MColor.kLabel.neutral),
-            const SizedBox(height: 8),
+  Widget build(BuildContext context) {
+    if (rankingList.isEmpty) {
+      return const Center(child: Text("랭킹 데이터가 없습니다."));
+    }
 
-            Card(
-              elevation: 0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(40),
-                side: BorderSide(color: MColor.kLine.normal),
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          MText.h2("이번 주 랭킹", color: MColor.kLabel.neutral),
+          const SizedBox(height: 8),
+          Card(
+            elevation: 0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(40),
+              side: BorderSide(color: MColor.kLine.normal),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 22,
+                top: 12,
+                right: 22,
+                bottom: 12,
               ),
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  left: 22,
-                  top: 12,
-                  right: 22,
-                  bottom: 12,
-                ),
-                child: SizedBox(
-                  height: 30,
-                  child: CarouselSlider(
-                    options: CarouselOptions(
-                      height: 30,
-                      viewportFraction: 1,
-                      autoPlay: true,
-                      autoPlayInterval: const Duration(seconds: 2),
-                      scrollDirection: Axis.vertical,
-                    ),
-                    items: rankingList.map((rank) {
-                      return Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              MText.h5(
-                                "${rank.rank}위",
-                                color: MColor.kLabel.neutral,
-                              ),
-                              const SizedBox(width: 16),
-                              MText.h5(rank.username, color: MColor.kLabel.normal),
-                              const SizedBox(width: 4),
-                              MText.buttonS(
-                                "[${rank.currentScore}점]",
-                                color: MColor.kLabel.normal,
-                              ),
-                            ],
-                          ),
-                          Container(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 12,
-                              vertical: 4,
-                            ),
-                            child: MText.buttonSS(
-                              "${rank.delta}↑",
-                              color: MColor.kButton.active,
-                            ),
-                          ),
-                        ],
-                      );
-                    }).toList(),
+              child: SizedBox(
+                height: 30,
+                child: CarouselSlider(
+                  options: CarouselOptions(
+                    height: 30,
+                    viewportFraction: 1,
+                    autoPlay: true,
+                    autoPlayInterval: const Duration(seconds: 2),
+                    scrollDirection: Axis.vertical,
                   ),
+                  items: rankingList.map((rank) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            MText.h5(
+                              "${rank.rank}위",
+                              color: MColor.kLabel.neutral,
+                            ),
+                            const SizedBox(width: 16),
+                            MText.h5(rank.username, color: MColor.kLabel.normal),
+                            const SizedBox(width: 4),
+                            MText.buttonS(
+                              "[${rank.currentScore}점]",
+                              color: MColor.kLabel.normal,
+                            ),
+                          ],
+                        ),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 12,
+                            vertical: 4,
+                          ),
+                          child: MText.buttonSS(
+                            "${rank.delta}↑",
+                            color: MColor.kButton.active,
+                          ),
+                        ),
+                      ],
+                    );
+                  }).toList(),
                 ),
               ),
             ),
-          ],
-        ),
-      );
-    }
+          ),
+        ],
+      ),
+    );
   }
 }
