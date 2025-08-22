3e3fcf8e3ebf8ea50d42055e61327e0ff9e10a9d
Seo Hoejeong <139827011+clubnerdy@users.noreply.github.com>
2025-08-22 17:15:56 +0900

    [fix] ui/workspace 모바일/태블릿 수정 (#27)
    - workspace 웹뷰 ui 수정

--- lib/ui/ma_page/holder/workspace/widget/ma_workspace_body.dart
+++ lib/ui/ma_page/holder/workspace/widget/ma_workspace_body.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:jjava_flutter/ui/ma_page/holder/workspace/widget/ma_workspace_compile_animation.dart';
-import 'package:jjava_flutter/ui/ma_page/holder/workspace/widget/ma_workspace_terminal.dart';
 import 'package:jjava_flutter/ui/ma_page/holder/workspace/widget/ma_workspace_web_view.dart';
 
 class MaWorkspaceBody extends StatefulWidget {
@@ -9,24 +7,8 @@ class MaWorkspaceBody extends StatefulWidget {
 }
 
 class _MaWorkspaceBodyState extends State<MaWorkspaceBody> {
-  bool _isLoading = false;
-  void _setLoading(bool v) => setState(() => _isLoading = v);
-
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Column(
-          children: [
-            Expanded(child: MaWorkspaceWebView(onLoading: _setLoading)),
-            MaWorkspaceTerminal(),
-          ],
-        ),
-
-        // 컴파일 애니메이션 UI
-        if (_isLoading) MaWorkspaceCompileAnimation(),
-        //
-      ],
-    );
+    return Expanded(child: MaWorkspaceWebView());
   }
 }
