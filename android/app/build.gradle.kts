64e71c8ea4978620253c6c10c15d1bac5ed8ee5d
hahamik <dlapdl0615@gmail.com>
2025-08-27 21:01:12 +0900

    1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,13 +3,12 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
-
-
 
 android {
     namespace = "com.example.jjava_flutter"
-    compileSdk = 36
+    compileSdk = 33   // 안정 버전
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -21,23 +20,29 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.jjava_flutter"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion     // 직접 숫자로 지정
-        targetSdk = 33
+        minSdkVersion flutter.minSdkVersion           // Flutter 최소 지원 버전
+        targetSdk = 33    // 최적화 대상
         versionCode = 1
         versionName = "1.0"
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    // Firebase BOM (모든 Firebase SDK 버전 관리용)
+    implementation(platform("com.google.firebase:firebase-bom:34.1.0"))
+
+    // Firebase Analytics
+    implementation("com.google.firebase:firebase-analytics")
+
+    // Firebase Authentication 예시
+    implementation("com.google.firebase:firebase-auth")
 }
 
 flutter {
